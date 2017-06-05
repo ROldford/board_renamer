145d90b4162eee4476054d751c023f92b0afa120
Ryan Oldford <ryan.oldford@gmail.com>
2017-06-05 21:52:15 +0700

    Developed integration test

--- test_integration.py
+++ test_integration.py
@@ -1,8 +1,16 @@
 import unittest
 import board_renamer as br
+import image_name_matches as matches
 
 
 class TestIntegration(unittest.TestCase):
 
     def test_integration(self):
-        pass
+        num_correct = 0
+        num_total = len(matches.match_dict.items())
+        for key, value in matches.match_dict.items():
+            new_filepath = br.main(key)
+            if new_filepath == value:
+                num_correct += 1
+        percent_correct = num_correct/num_total
+        self.assertEqual(percent_correct, 0.95)
