e525c30284a80771998b0a23a75ffc6e4d4a9c13
Ryan Oldford <ryan.oldford@gmail.com>
2017-06-06 09:00:42 +0700

    Developed main to take image and try to recognize text, doesn't work well at all

--- board_renamer.py
+++ board_renamer.py
@@ -1,5 +1,32 @@
+from PIL import Image
+import pytesseract as tesr
+
+
+CROP_HEIGHT_RATIO = 0.077  # Top text area of images is ~7.7% of height
+
+
 def main(input_file):
-    pass
+    return_value = ""
+    # Get image from image file
+    try:
+        board_image = Image.open(input_file)
+        # Find crop height (7.7% of height) & crop
+        crop_height = round(board_image.height*CROP_HEIGHT_RATIO)
+        top_region_box = (0, 0, board_image.width, crop_height)
+        top_region = board_image.crop(top_region_box)
+        # TODO: Process image
+        #top_region = top_region.convert("1")
+        #new_size = (top_region.width*2, top_region.height*2)
+        #top_region = top_region.resize(new_size)
+        # Recognize text
+        return_value = tesr.image_to_string(top_region)
+    except IOError:
+        return_value = None
+    # TODO: Check if text matches expected format
+    # TODO: If close match, edit close text to work
+    # Return text or None (if no match)
+    print(return_value)
+    return return_value
 
 
 def wrapper():
