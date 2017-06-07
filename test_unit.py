65c292d5800a41cc70d8dfcc8bcdfeda9a16663b
Ryan Oldford <ryan.oldford@gmail.com>
2017-06-07 14:56:07 +0700

    Added placeholder unit test file

---
+++ test_unit.py
@@ -0,0 +1,14 @@
+import unittest
+import board_renamer as br
+
+
+class TestOnlyBlack(unittest.TestCase):
+
+    def test_colors_to_white(self):
+        pass
+
+    def test_grey_to_white(self):
+        pass
+
+    def test_black_to_black(self):
+        pass
